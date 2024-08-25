c9f033be0c7201e63256c884e8e29f5c68a1e79d
nithyasarathim <nithyasarathi.m2023cse@sece.ac.in>
2024-08-25 14:20:47 +0530

    Errors fixed and css updated !

--- digital clock project/index.js
+++ digital clock project/index.js
@@ -2,6 +2,10 @@ function updateClock() {
     const now = new Date();
     const hrs = now.getHours().toString().padStart(2, '0');
     const mins = now.getMinutes().toString().padStart(2, '0');
-    const secs = new.getSeconds().toString().padStart(2, '0');
-    const time ='${hrs}:${mins}:${secs}';
+    const secs = now.getSeconds().toString().padStart(2, '0');
+    const time = `${hrs}:${mins}:${secs}`;
+    document.getElementById('clock').textContent = time;
 }
+
+updateClock();
+setInterval(updateClock, 1000);
