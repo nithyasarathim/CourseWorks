42131093f8e410306f0ddfb73856294e7c1d79cb
nithyasarathim <nithyasarathi.m2023cse@sece.ac.in>
2024-08-26 15:22:07 +0530

    Added JS with API support

---
+++ Currency_convertor/script.js
@@ -0,0 +1,26 @@
+let btn = document.getElementById('convert-btn');
+let input = document.getElementById("from-currency");
+let output= document.getElementById("to-currency");
+btn.addEventListener('click',() =>{
+    const curr1 =input.value;
+    const curr2 =output.value; 
+    let val = Number(document.getElementById("amount").value);
+    if(curr1 === curr2)
+    {
+        document.getElementById('alert').textContent=`Currency shouldn't be same !`;
+    }
+    else
+    {
+        document.getElementById('alert').textContent=``;
+        convert(curr1,curr2,val);
+    }
+})
+function convert(curr1,curr2,val)
+{
+    const host = 'api.frankfurter.app';
+    fetch(`https://${host}/latest?amount=${val}&from=${curr1}&to=${curr2}`)
+      .then(resp => resp.json())
+      .then((data) => {
+        document.getElementById(`result`).textContent=`${val} ${curr1} = ${data.rates[curr2]}  ${curr2}`;
+      });
+}
