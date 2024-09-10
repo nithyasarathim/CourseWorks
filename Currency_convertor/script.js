05841394a821726bb6551b65b12c8f8e813ad569
nithyasarathim <nithyasarathi.m2023cse@sece.ac.in>
2024-09-10 10:44:06 +0530

    Includer async and await function

--- Currency_convertor/script.js
+++ Currency_convertor/script.js
@@ -15,12 +15,16 @@ btn.addEventListener('click',() =>{
         convert(curr1,curr2,val);
     }
 })
-function convert(curr1,curr2,val)
+async function convert(curr1,curr2,val)
 {
     const host = 'api.frankfurter.app';
-    fetch(`https://${host}/latest?amount=${val}&from=${curr1}&to=${curr2}`)
+   try{await fetch(`https://${host}/latest?amount=${val}&from=${curr1}&to=${curr2}`)
       .then(resp => resp.json())
       .then((data) => {
         document.getElementById(`result`).textContent=`${val} ${curr1} = ${data.rates[curr2]}  ${curr2}`;
-      });
+      });}
+      catch(err){
+        convert(curr1,curr2,val);
+      }
+
 }
